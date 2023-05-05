94dffafc46051607a485db6e0f954ceb952559ad
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-05 17:05:35 +0600

    chagne hero image and add shadow testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import Avatar from "../assets/avatar.svg";
+import TestimonialBgImg1 from "../assets/testimonial1.svg";
+import TestimonialBgImg2 from "../assets/testimonial2.svg";
 
 const Testimonial = () => {
   return (
-    <div className='w-[70%] xl:w-[50%] mx-auto mb-40'>
-      <h3 className='text-3xl md:text-4xl lg:text-[2rem] font-medium'>“The process of adding and allocating guards to site easier than never before”</h3>
-      <div className='flex items-center mt-10'>
-        <hr className='border border-black mr-5 w-60' />
-        <div className='flex items-center'>
-          <img src={Avatar} alt="avatar" />
-          <div className='ml-5'>
-            <h4 className='text-lg font-bold'>Name</h4>
-            <p className='text-lg text-[#4E4E4E]'>Raftaar Security Services</p>
+    <div className='w-[70%] xl:w-[50%] mx-auto mb-40 flex items-center justify-start'>
+      <div className='absolute'>
+        <img className='w-80 h-80 absolute -top-16 right-36' src={TestimonialBgImg1} alt="" />
+        <img className='w-80 h-80 relative right-10' src={TestimonialBgImg2} alt="" />
+      </div>
+      <div className='relative'>
+        <h3 className='text-3xl md:text-4xl lg:text-[2rem] font-medium'>“The process of adding and allocating guards to site easier than never before”</h3>
+        <div className='flex items-center mt-10'>
+          <hr className='border border-black mr-5 w-60' />
+          <div className='flex items-center'>
+            <img src={Avatar} alt="avatar" />
+            <div className='ml-5'>
+              <h4 className='text-lg font-bold'>Name</h4>
+              <p className='text-lg text-[#4E4E4E]'>Raftaar Security Services</p>
+            </div>
           </div>
         </div>
       </div>
